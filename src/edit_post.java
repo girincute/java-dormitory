aab57b893cea735d6d83dff907815537be8a4c4b
Sunyoung Shin <s2017i30@e-mirim.hs.kr>
2018-11-24 00:18:39 +0900

    + change cleanroom, add plus_and_minus UI in teacher_maiin

--- src/edit_post.java
+++ src/edit_post.java
@@ -1,5 +1,5 @@
-// 포스트들에 관련된 (공지사항 더보기, 상벌점 더보기, 청소구역 전체보기 ) UI
 
+// 포스트들에 관련된 (공지사항 더보기, 상벌점 더보기, 청소구역 전체보기 ) UI
 
 import java.awt.*;
 import java.awt.event.*;
@@ -10,26 +10,44 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.event.*;
 
-
 class plus_and_minus_edit_UI extends JFrame { // 상벌점 추가/삭제
-	
+
 	plus_and_minus_edit_UI() {
 
 		this.setTitle("Dormitory Management System_write_laundry");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(null);
 
+		JTextField why = new JTextField(); // 항목
+		why.setBounds(460, 265, 250, 40);
+		this.add(why);
 		
-		JLabel index = new JLabel("상벌점 수정");
-		index.setFont(new Font("나눔고딕", 50, 50)); // 폰트 설정
-		index.setBounds(170, 200, 600, 70);
-		this.add(index);
+		JTextField plus = new JTextField(); // 상점
+		plus.setBounds(460, 330, 70, 40);
+		this.add(plus);
 		
-//		ImageIcon backicon = new ImageIcon("img//write_out_back.jpg");
-//		JLabel backlabel = new JLabel(backicon);
-//		backlabel.setBounds(0, 0, 1080, 720);
-//		this.add(backlabel);
+		JTextField minus = new JTextField(); // 벌점
+		minus.setBounds(460, 400, 70, 40);
+		this.add(minus);
 		
+		// 변경 버튼
+				ImageIcon changbtn = new ImageIcon("img//changbtn.png");
+				JLabel changbtn_label = new JLabel(changbtn);
+				changbtn_label.setBounds(460, 560, 170, 70);
+				this.add(changbtn_label);
+
+				changbtn_label.addMouseListener(new MouseAdapter() {
+					public void mouseClicked(MouseEvent e) {
+						// 변경버튼 클릭했을 때
+					}
+				});
+		
+		// 백그라운드 (배경)
+		ImageIcon backicon = new ImageIcon("img//add_plus_and_minus_back.jpg");
+		JLabel backlabel = new JLabel(backicon);
+		backlabel.setBounds(0, 0, 1080, 720);
+		this.add(backlabel);
+
 		this.pack();
 		this.setSize(1080, 720);
 		this.setVisible(true);
@@ -40,27 +58,41 @@ class plus_and_minus_edit_UI extends JFrame { // 상벌점 추가/삭제
 }
 
 class classroom_edit_UI extends JFrame { // 청소구역 수정
-	
+
 	classroom_edit_UI() {
 
 		this.setTitle("Dormitory Management System_write_laundry");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(null);
 
-		
-		JLabel index = new JLabel("청소구역 수정");
-		index.setFont(new Font("나눔고딕", 50, 50)); // 폰트 설정
-		index.setBounds(170, 200, 600, 70);
-		this.add(index);
-		
-//		ImageIcon backicon = new ImageIcon("img//write_out_back.jpg");
-//		JLabel backlabel = new JLabel(backicon);
-//		backlabel.setBounds(0, 0, 1080, 720);
-//		this.add(backlabel);
-		
-		// this.setResizable(false); // 프로그램을 함부로 크기 조정할 수 있나?
-		// this.setPreferredSize(new Dimension(1080, 1080 / 12 * 9)); // 왼쪽, 오른쪽 둘 다 넣을
-		// 수 있게 하는게 디맨션
+		String clean_where[] = { "세면실", "샤워실", "세탁실", "화장실", "복도", "계단", "휴게실" };
+
+		JComboBox room_arr = new JComboBox(clean_where);
+		room_arr.setBounds(570, 285, 100, 40);
+		this.add(room_arr);
+
+		JTextField search = new JTextField();
+		search.setBounds(570, 375, 100, 40);
+		this.add(search);
+
+		// 변경 버튼
+		ImageIcon changbtn = new ImageIcon("img//changbtn.png");
+		JLabel changbtn_label = new JLabel(changbtn);
+		changbtn_label.setBounds(460, 560, 170, 70);
+		this.add(changbtn_label);
+
+		changbtn_label.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				// 변경버튼 클릭했을 때
+			}
+		});
+
+		// 백그라운드
+		ImageIcon backicon = new ImageIcon("img//change_cleanroom_back.jpg");
+		JLabel backlabel = new JLabel(backicon);
+		backlabel.setBounds(0, 0, 1080, 720);
+		this.add(backlabel);
+
 		this.pack();
 		this.setSize(1080, 720);
 		this.setVisible(true);
@@ -71,36 +103,36 @@ class classroom_edit_UI extends JFrame { // 청소구역 수정
 }
 
 class notice_edit_UI extends JFrame { // 공지사항 수정
-	
+
 	notice_edit_UI() {
 
 		this.setTitle("Dormitory Management System_write_laundry");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(null);
 
-		//공지사항 내용
+		// 공지사항 내용
 		JTextField search = new JTextField();
 		search.setBounds(120, 150, 840, 410);
 		this.add(search);
-		
-		//변경 버튼
+
+		// 변경 버튼
 		ImageIcon changbtn = new ImageIcon("img//changbtn.png");
 		JLabel changbtn_label = new JLabel(changbtn);
 		changbtn_label.setBounds(420, 590, 170, 70);
 		this.add(changbtn_label);
-		
+
 		changbtn_label.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				// 변경버튼 클릭했을 때
 			}
 		});
-		
+
 		// 배경
 		ImageIcon backicon = new ImageIcon("img//upload_notice.jpg");
 		JLabel backlabel = new JLabel(backicon);
 		backlabel.setBounds(0, 0, 1080, 720);
 		this.add(backlabel);
-		
+
 		this.pack();
 		this.setSize(1080, 720);
 		this.setVisible(true);
@@ -110,9 +142,8 @@ class notice_edit_UI extends JFrame { // 공지사항 수정
 	}
 }
 
-
 public class edit_post {
 	public static void main(String[] args) {
-		new notice_edit_UI();
+		new plus_and_minus_edit_UI();
 	}
 }
